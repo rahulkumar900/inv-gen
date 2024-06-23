54339921541e65381b032d9e3c92582970544fdf
rahulkumar900 <rahulkumar900620@gmail.com>
2024-06-24 01:37:10 +0530

    adding new template

--- src/pdfTemplate/firstTemplate/oneTemplate.tsx
+++ src/pdfTemplate/firstTemplate/oneTemplate.tsx
@@ -1,48 +1,341 @@
 import React from "react";
-import { Document, Text, View, Page } from "@react-pdf/renderer";
+import { Document, Text, View, Page, StyleSheet } from "@react-pdf/renderer";
 import { useTheme } from "@/pdfComponent/themeContext";
+import { Description } from "@radix-ui/react-dialog";
+import { FileX } from "lucide-react";
+
+const invoices = [
+  { description: "Ipad Mini", price: 10, qty: 2, amount: 20 },
+  { description: "Ipad Mini", price: 10, qty: 2, amount: 20 },
+  { description: "Ipad Mini", price: 10, qty: 2, amount: 20 },
+  { description: "Ipad Mini", price: 10, qty: 2, amount: 20 },
+  { description: "Ipad Mini", price: 10, qty: 2, amount: 20 },
+  // { description: "Ipad Mini", price: 10, qty: 2, amount: 20 },
+];
+
+function Tax() {
+  return (
+    <>
+      <View
+        style={{
+          flexDirection: "row",
+          fontStyle: "bold",
+          fontSize: 12,
+          paddingHorizontal: 4,
+          paddingTop: 28,
+        }}
+      >
+        <View style={{ width: "50%" }}>
+          <Text style={{ textAlign: "left", fontStyle: "uppercase" }}>
+            SUBTOTAL :
+          </Text>
+        </View>
+        <View style={{ width: "50%" }}>
+          <Text style={{ textAlign: "center" }}>5225</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          fontStyle: "bold",
+          paddingVertical: 7,
+          paddingHorizontal: 4,
+          marginVertical: 7,
+          fontSize: 12,
+
+          // paddingTop: 4,
+        }}
+      >
+        <View style={{ width: "50%" }}>
+          <Text style={{ textAlign: "left", fontStyle: "uppercase" }}>
+            TAX :
+          </Text>
+        </View>
+        <View style={{ width: "50%" }}>
+          <Text style={{ textAlign: "center" }}>25</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          fontStyle: "bold",
+          // flex: 1,
+          color: "white",
+
+          fontSize: 12,
+          backgroundColor: "#464097",
+          paddingVertical: 7,
+          marginVertical: 7,
+          paddingHorizontal: 4,
+          // paddingTop: 7,
+        }}
+      >
+        <View style={{ width: "50%" }}>
+          <Text style={{ textAlign: "left", fontStyle: "uppercase" }}>
+            TOTAL DUE :
+          </Text>
+        </View>
+        <View style={{ width: "50%" }}>
+          <Text style={{ textAlign: "center" }}>25585</Text>
+        </View>
+      </View>
+    </>
+  );
+}
+
+function Note() {
+  return (
+    <View style={{ paddingHorizontal: 14, paddingVertical: 28 }}>
+      <Text>Note : </Text>
+      <Text
+        style={{
+          fontFamily: "Helvetica",
+          fontSize: 11,
+          marginTop: 4,
+          lineHeight: 1.2,
+          textOverflow: "ellipsis",
+        }}
+      >
+        Payment should be within 15 days of delivery,React-pdf is shipped with a
+        Font module that enables to load fonts from different sources, handle
+        how words are wrapped and defined an emoji source to embed these glyphs
+        on your document.
+      </Text>
+    </View>
+  );
+}
+
+function Row({
+  description,
+  price,
+  qty,
+  amount,
+}: {
+  description: string;
+  price: number;
+  qty: number;
+  amount: number;
+}) {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        fontStyle: "bold",
+        // marginTop: "24",
+
+        // backgroundColor: "#464097",
+        // color: "white",
+        fontSize: 12,
+        padding: 5,
+        borderBottom: 1,
+      }}
+    >
+      <View style={{ width: "40%", padding: 14 }}>
+        <Text>{description}</Text>
+      </View>
+      <View style={{ width: "20%", padding: 14 }}>
+        <Text>{price}</Text>
+      </View>
+      <View style={{ width: "20%", padding: 14 }}>
+        <Text style={{ textAlign: "center" }}>{qty}</Text>
+      </View>
+      <View style={{ width: "20%", padding: 14 }}>
+        <Text style={{ textAlign: "center" }}>{amount}</Text>
+      </View>
+    </View>
+  );
+}
+
+function Table() {
+  return (
+    <>
+      {invoices.map((invoice, i) => (
+        <Row key={i} {...invoice} />
+      ))}
+    </>
+  );
+}
 
 export default function One() {
-  const styles = useTheme();
   return (
     <Document>
-    <Page style={[styles.body,{color:"blue"}]}>
-      
-      <View style={{display:"flex",flexDirection: "row"}}>
-      <View style={{width:"50%"}}>
-        <View style={{width:60,height: 60,border:1,}}>q</View>
-        <Text style={{ fontFamily: 'Courier-Bold',color:"blue",marginTop: 8,fontSize:16}}>NALANDA ENGICON PVT LTD</Text>
+      <Page
+        style={{
+          paddingTop: 35,
+          paddingBottom: 35,
+          paddingHorizontal: 35,
+          fontFamily: "Helvetica",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          <View style={{ width: "50%" }}>
+            <View style={{ width: 60, height: 60, border: 1 }}>q</View>
+            <Text
+              style={{
+                fontFamily: "Helvetica-Bold",
+                color: "#464097",
+                marginTop: 8,
+                fontSize: 16,
+              }}
+            >
+              NALANDA ENGICON PVT LTD
+            </Text>
+          </View>
+          <View style={{ width: "50%", paddingLeft: 60 }}>
+            <Text
+              style={{
+                fontSize: 24,
+                maxWidth: "80%",
+                color: "#464097",
+                fontFamily: "Helvetica-Bold",
+                marginBottom: 4,
+                letterSpacing: 4,
+              }}
+            >
+              INVOICE
+            </Text>
+            <Text style={{ fontSize: 14, fontFamily: "Helvetica-Bold" }}>
+              December 06, 2024
+            </Text>
+          </View>
         </View>
-      <View style={{width:"50%",paddingLeft:60}}>
-        <Text style={{fontSize: 24,maxWidth:"80%", fontFamily:"Courier-Bold",}}>INVOICE</Text>
-        <Text style={{fontSize:14}}>22-06-2024</Text>
+
+        <View style={{ display: "flex", flexDirection: "row", marginTop: 24 }}>
+          <View style={{ width: "50%" }}>
+            <Text
+              style={{
+                fontSize: 12,
+                marginBottom: 7,
+                fontFamily: "Helvetica-Bold",
+              }}
+            >
+              Office Address
+            </Text>
+            <Text style={{ fontSize: 12, maxWidth: "70%", lineHeight: 1.7 }}>
+              4th Floor N.P Tower Kankarbagh Patna, 80020 bla bla
+            </Text>
+          </View>
+
+          <View style={{ width: "50%", paddingLeft: 60, lineHeight: 1.7 }}>
+            <Text
+              style={{
+                fontSize: 12,
+                marginBottom: 7,
+                fontFamily: "Helvetica-Bold",
+              }}
+            >
+              To:
+            </Text>
+            <Text style={{ fontSize: 12, maxWidth: "70%" }}>
+              4th Floor N.P Tower Kankarbagh Patna 80020
+            </Text>
+          </View>
         </View>
-      </View>
-      
-      <View  style={{display:"flex",flexDirection: "row",marginTop: 12}}>
-         <View style={{width:"50%"}}>
-          
-              <Text style={{fontSize:12,marginBottom:4,fontFamily:"Courier-Bold"}}>Office Address</Text>
-           <Text style={{fontSize:12,maxWidth: "70%"}}>4th Floor N.P Tower 
-             Kankarbagh Patna, 80020 bla bla
-           </Text>
-          
-       
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            fontFamily: "Helvetica-Bold",
+            fontStyle: "bold",
+            marginTop: "24",
+
+            backgroundColor: "#464097",
+            color: "white",
+            fontSize: 12,
+            // padding: 4,
+          }}
+        >
+          <View style={{ width: "40%", padding: 10 }}>
+            <Text>Items Description</Text>
+          </View>
+          <View style={{ width: "20%", padding: 10 }}>
+            <Text>Unit Price</Text>
+          </View>
+          <View style={{ width: "20%", padding: 10 }}>
+            <Text style={{ textAlign: "center" }}>Qty</Text>
+          </View>
+          <View style={{ width: "20%", padding: 10 }}>
+            <Text style={{ textAlign: "center" }}>Amount</Text>
+          </View>
         </View>
-      
-       <View style={{width:"50%",paddingLeft:60}}>
-        <Text style={{fontSize:12,marginBottom:4}}>To:</Text>
-           <Text style={{fontSize:12,maxWidth: "70%"}}>4th Floor N.P Tower 
-             Kankarbagh Patna 80020
-           </Text>
+
+        <Table />
+        <view
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            fontFamily: "Helvetica-Bold",
+            fontStyle: "bold",
+            width: "100%",
+            height: "auto",
+            fontSize: 13,
+            // backgroundColor: "blue"
+          }}
+        >
+          <View
+            style={{
+              width: "60%",
+
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              overflow: "hidden",
+            }}
+          >
+            <Note />
+          </View>
+          <view style={{ width: "40%", height: "100%" }}>
+            <Tax />
+          </view>
+        </view>
+
+        <View
+          style={{
+            flexDirection: "row",
+            padding: 14,
+            height: "130",
+          }}
+        >
+          <View
+            style={{
+              width: "60%",
+              display: "flex",
+
+              flexDirection: "column",
+              height: "100%",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
+              color: "#464097",
+              flex: 1,
+            }}
+          >
+            <Text style={{ fontSize: 18, fontFamily: "Helvetica-Bold" }}>
+              Thanks for your Business
+            </Text>
+          </View>
+          <View
+            style={{
+              width: "40%",
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Text>Signature</Text>
+          </View>
         </View>
-      </View>
-        
-     
-      <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
-        `${pageNumber} / ${totalPages}`
-      )} fixed />
-    </Page>
-  </Document>
+
+        {/* <Text
+          style={styles.pageNumber}
+          render={({ pageNumber, totalPages }) =>
+            `${pageNumber} / ${totalPages}`
+          }
+          fixed
+        /> */}
+      </Page>
+    </Document>
   );
 }
